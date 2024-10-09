088bb39cfc4a89c1e2053403a3736f28ce3e4d8f
dreamkanathip <142082066+dreamkanathip@users.noreply.github.com>
2024-10-09 14:39:56 +0700

    add be function & prisma

---
+++ backend/routes/product.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+// Controllers
+const {
+  create,
+  list,
+  read,
+  update,
+  remove,
+  listby,
+  searchFilters,
+} = require("../controllers/product");
+
+// @ENDPOINT http://localhost:5000/api/product
+router.post("/product", create);
+router.get("/products/:count", list);
+router.get("/product/:id", read);
+router.put("/product/:id", update);
+router.delete("/product/:id", remove);
+router.post("/productby", listby);
+router.post("/search/filters", searchFilters);
+
+module.exports = router;
