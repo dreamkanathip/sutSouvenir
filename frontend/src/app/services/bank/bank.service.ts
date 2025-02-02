88db2136b7ae7b31eee3122ff6beefadd0edc53e
dreamkanathip <142082066+dreamkanathip@users.noreply.github.com>
2025-02-02 16:35:14 +0700

    update bank

--- frontend/src/app/services/bank/bank.service.ts
+++ frontend/src/app/services/bank/bank.service.ts
@@ -2,37 +2,50 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BankService {
+  apiUrl = 'http://localhost:5000/api'; // URL ของ API
 
-  apiUrl = "http://localhost:5000/api"
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
-   }
+  // ฟังก์ชันสำหรับดึงข้อมูลธนาคารปลายทาง
+  getDestBank() {
+    return this.http.get<any>(`${this.apiUrl}/destBank`);
+  }
 
-   getDestBank() {
-    return this.http.get<any>(`${this.apiUrl}/destBank`)
-   }
-   getOriginBank() {
-    return this.http.get<any>(`${this.apiUrl}/originBank`)
-   }
-   addDestBank(data: any) {
-    return this.http.post<any>(`${this.apiUrl}/destBank`, data)
-   }
-   addOriginBank(data: any) {
-    return this.http.post<any>(`${this.apiUrl}/originBank`, data)
-   }
-   updateDestBank(id: number) {
-    return this.http.get<any>(`${this.apiUrl}/destBank/${id}`)
-   }
-   updateOriginBank(id: number) {
-    return this.http.get<any>(`${this.apiUrl}/originBank/${id}`)
-   }
+  // ฟังก์ชันสำหรับดึงข้อมูลธนาคารต้นทาง
+  getOriginBank() {
+    return this.http.get<any>(`${this.apiUrl}/originBank`);
+  }
+
+  // ฟังก์ชันสำหรับเพิ่มข้อมูลธนาคารปลายทาง
+  addDestBank(data: any) {
+    return this.http.post<any>(`${this.apiUrl}/destBank`, data);
+  }
+
+  // ฟังก์ชันสำหรับเพิ่มข้อมูลธนาคารต้นทาง
+  addOriginBank(data: any) {
+    return this.http.post<any>(`${this.apiUrl}/originBank`, data);
+  }
+
+  // ฟังก์ชันสำหรับดึงข้อมูลสำหรับการอัปเดตธนาคารปลายทาง
+  updateDestBank(id: number) {
+    return this.http.get<any>(`${this.apiUrl}/destBank/${id}`);
+  }
+
+  // ฟังก์ชันสำหรับดึงข้อมูลสำหรับการอัปเดตธนาคารต้นทาง
+  updateOriginBank(id: number) {
+    return this.http.get<any>(`${this.apiUrl}/originBank/${id}`);
+  }
+
+  // ฟังก์ชันสำหรับลบข้อมูลธนาคารปลายทาง
   deleteDestBank(id: number) {
-    return this.http.get<any>(`${this.apiUrl}/destBank/${id}`)
-   }
-   deleteOriginBank(id: number) {
-    return this.http.get<any>(`${this.apiUrl}/originBank/${id}`)
-   }
+    return this.http.delete<any>(`${this.apiUrl}/destBank/${id}`);
+  }
+
+  // ฟังก์ชันสำหรับลบข้อมูลธนาคารต้นทาง
+  deleteOriginBank(id: number) {
+    return this.http.delete<any>(`${this.apiUrl}/originBank/${id}`);
+  }
 }
